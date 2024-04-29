785b15af92c3880f651d9d286ed20cd85d469ac4
Rarsa-company <7266099-rosa.ap@users.noreply.gitlab.com>
2024-04-29 13:14:08 +0700

    fixing controller add response

--- controller.js
+++ controller.js
@@ -4,5 +4,5 @@ let response = require('./rest');
 let connection = require('./koneksi');
 
 exports.index = function(req,res){
-    response.ok("App Running Success")
+    response.ok("App Running Success", res)
 };
